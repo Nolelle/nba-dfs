92bb19a720077799a90b91984b77a8e72dd61fa5
Edmund <eyu1@ualberta.ca>
2022-05-25 17:55:02 -0600

    working next tutorial

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link';
 
 const Home: NextPage = () => {
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,6 +16,10 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
+          Read{' '}
+          <Link href= "/posts/first-post">
+            <a>this page!</a> 
+          </Link>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
