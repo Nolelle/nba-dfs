d446772dbd2e13ef021df42a730c3d85cb4b2cb1
Edmund <eyu1@ualberta.ca>
2022-05-26 17:44:11 -0600

    next.js tutorial

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,21 +1,22 @@
 import Link from "next/link";
 import Head from "next/head";
 import Script from "next/script";
+import Layout from "../../components/layout"
 
 const FirstPost = () => {
   return (
-    <>
-    <Head>
-      <title>First Post</title>
-    </Head>
-       <h1>First Post</h1>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
       <h2>
-        <Link href='/'>
-          <a>Back to Home</a>
+        <Link href="/">
+          <a>Back to home</a>
         </Link>
       </h2>
-    </>
-  )
+    </Layout>
+  );
 }
 
 export default FirstPost
